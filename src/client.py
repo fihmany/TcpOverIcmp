1d564eadfe37a80cbaca41f5dca16f5f18d6dff7
razg <razgino11@gmail.com>
2022-10-29 16:48:08 +0300

    sync

--- src/client.py
+++ src/client.py
@@ -2,20 +2,39 @@ from common.icmp import ICMPPacket
 import socket
 
 def main():
-
     print("Hello Client")
-    sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
 
-    #while True:
-    #msg = input("Enter message: ")
-    msg = "Hello World"
-    icmp = ICMPPacket(data=msg)
-    
-    sock.sendto(icmp.raw, ("127.0.0.1", 7))
+    tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    icmp_sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
 
-    # Receive data
-    #data, addr = sock.recvfrom(65535)
-    #print("Response: ", data)
+    tcp_client_add = ("127.0.0.1", 80)
+    icmp_server_add = ("127.0.0.1", 5353)
+
+    tcp_packet_max = 65535
+    icmp_packet_max = 65535
+
+    tcp_sock.bind(tcp_client_add)
+
+    # accept a connection from the tcp client
+    conn, addr = tcp_sock.accept()
+    print("Connected to: ", addr)
+
+    while True:
+        # Receive data from tcp client
+        data = conn.recv(tcp_packet_max)
+        print("Received from tcp client: ", data.decode("utf-8"))
+
+        # Format data as icmp packet and send to icmp server
+        icmp = ICMPPacket(data=data)
+        icmp_sock.sendto(icmp.raw, icmp_server_add)
+
+        # Receive response from icmp server
+        data, addr = icmp_sock.recvfrom(icmp_packet_max)
+        print("Response from icmp server: ", data)
+
+        # Send response to tcp client
+        conn.send(data)
+
 
 if __name__ == "__main__":
     main()
--- src/server.py
+++ src/server.py
@@ -5,26 +5,41 @@ from common.icmp import ICMPPacket, ext_icmp_header, IP_PACKET_SIZE, ICMP_HEADER
 def main():
     print("Hello Server")
 
-    sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
-    sock.bind(("127.0.0.1", 7))
+    target_addr = "https://raw.githubusercontent.com/fihmany/TcpOverIcmp/main/README.md"
+    target_addr_tuple = (target_addr, 80)
+    icmp_client_add = ("127.0.0.1", 5353)
+
+    icmp_sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
+
+    icmp_packet_max = 65535
 
     while True:
-        # Receive data
-        data, addr = sock.recvfrom(65535)
-        # print the received packet hex
-        icmp_pack_len = struct.calcsize(ICMP_STRUCTURE_FMT)
-        print(icmp_pack_len)
-        icmp = ext_icmp_header(data[IP_PACKET_SIZE:(IP_PACKET_SIZE+icmp_pack_len)])
-        print("Received Hex: ", data.hex())
-        # pritn data len
-        print("Received Data Len: ", len(data))
+        # Receive data from the icmp client
+        data, addr = icmp_sock.recvfrom(icmp_packet_max)
         
-        #struct_data = ICMPPacket(data)
-        print("server" + str(icmp))
-        print("Response: ", data[(IP_PACKET_SIZE+icmp_pack_len):])
+        # Print the received packet hex
+        icmp_len = struct.calcsize(ICMP_STRUCTURE_FMT)
+        icmp_header = ext_icmp_header(data[IP_PACKET_SIZE:(IP_PACKET_SIZE+icmp_len)])
+        icmp_payload = data[(IP_PACKET_SIZE+icmp_len):]
+        
+        print("Received Hex: ", data.hex())
+        print("Received Data Len: ", len(data))
+        print("Request Header", icmp_header)
+        print("Request Payload: ", icmp_payload)
 
-        #msg = input("Enter message: ")
-        #sock.sendto(str.encode(msg), ("127.0.0.1", 3232))
+        # Access the target address and send the received icmp payload to it
+        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sock.connect(target_addr_tuple)
+
+        # send the data to it
+        tcp_sock.send(icmp_payload)
+        tcp_resposnse = tcp_sock.recv(icmp_packet_max)
+        
+        # Format data as icmp packet and send it back to the icmp client
+        icmp_packet = ICMPPacket(data=tcp_resposnse)
+        icmp_sock.sendto(icmp_packet.raw, icmp_client_add)
+
+
 
 
 if __name__ == "__main__":
