087b40f6519dfe7772bfe8cfc80c46f8a6980e60
Yair <fihmany@gmail.com>
2022-12-03 19:00:35 +0200

    make server use select too

--- src/server.py
+++ src/server.py
@@ -1,28 +1,50 @@
+import select
 import socket
 from common.icmp import ICMPPacket, parse_icmp_packet, ICMP_ECHO_REQUEST, ICMP_ECHO_REPLY
 from common.data_types import IcmpOverTcpPacket
+from common.utils import ICMP_PACKET_MAX, TCP_PACKET_MAX
 
-def main():
-    print("Hello Server")
+class ProxyServer:
 
-    # target_addr = "www.example.com"
-    # target_addr_tuple = (target_addr, 80)
-    icmp_client_add = ("127.0.0.1", 5353)
+    def __init__(self):
+        # target_addr = "www.example.com"
+        # target_addr_tuple = (target_addr, 80)
+        self.icmp_client_addr = ("127.0.0.1", 5353)
+        self.icmp_sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
+        self.icmp_sock.bind(self.icmp_client_addr)
+        self.sockets = [self.icmp_sock]
 
-    icmp_sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
-    icmp_sock.bind(icmp_client_add)
+    def server_serve(self):
+        print("Hello Server")
 
-    icmp_packet_max = 65535
+        while True:
+            ready_to_read, _, _ = select.select(self.sockets, [], [])
+            for sock in ready_to_read:
+                if sock.protocol == socket.IPPROTO_ICMP:
+                    self.handle_icmp_packet(sock)
+                else:
+                    self.handle_tcp_packet(sock)
+            
+            
+    def handle_tcp_packet(self, sock: socket.socket):
+        tcp_resposnse = sock.recv(TCP_PACKET_MAX)
 
-    while True:
-        print ("waiting for icmp packet")
+        # print("Response from target: ", tcp_resposnse)
+        # Format data as icmp packet and send it back to the icmp client
+        icmp_packet = ICMPPacket(icmp_type=ICMP_ECHO_REPLY, data=tcp_resposnse)
+        self.icmp_sock.sendto(icmp_packet.raw, self.icmp_client_addr)
+        if (sock in self.sockets):
+            sock.close()
+            self.sockets.remove(sock)
+
+    def handle_icmp_packet(self, sock: socket.socket):
         # Receive data from the icmp client
-        data, addr = icmp_sock.recvfrom(icmp_packet_max)
+        data, _ = sock.recvfrom(ICMP_PACKET_MAX)
         # print (data)
         # Print the received packet hex
         icmp_packet = parse_icmp_packet(data)
         if (icmp_packet.icmp_type != ICMP_ECHO_REQUEST):
-            continue
+            return
 
         # Pase the data as an icmp over tcp packet
         icmp_over_tcp = IcmpOverTcpPacket()
@@ -43,22 +65,16 @@ def main():
         #print("Request Payload: ", icmp_packet.data)
 
         # Access the target address and send the received icmp payload to it
-        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sock = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 
-        # TODO RAZ: mabye pass the port of connection as well ?
+        #TODO RAZ: mabye pass the port of connection as well ?
         tcp_sock.connect((target_addr, 80))
 
         # send the data to it
         tcp_sock.send(data_to_send)
-        tcp_resposnse = tcp_sock.recv(icmp_packet_max)
+        self.sockets.append(tcp_sock)
 
-        # print("Response from target: ", tcp_resposnse)
-        
-        # Format data as icmp packet and send it back to the icmp client
-        icmp_packet = ICMPPacket(icmp_type=ICMP_ECHO_REPLY, data=tcp_resposnse)
-        icmp_sock.sendto(icmp_packet.raw, icmp_client_add)
-
-        # create tcp client
 
 if __name__ == "__main__":
-    main()
+    server = ProxyServer()
+    server.server_serve()
